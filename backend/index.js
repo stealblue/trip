45039933e8a148815612e2f278441ba025f157f7
권미애 <alxo9974@naver.com>
2023-06-28 17:53:12 +0900

    게시판 insert기능 구현중

--- backend/index.js
+++ backend/index.js
@@ -68,5 +68,16 @@ const server = app.listen(PORT || 4000, () => {
   console.log(`Listening to port ${PORT}`);
 });
 
+app.use((req, res, next) => {
+  const err = new Error("NOT FOUND");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.render("error");
+});
+
 webSocket(server, app, sessionMiddleware);
 // webSocket();
