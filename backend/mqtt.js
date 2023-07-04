a159b89af56500713b6e9c06f058abee76cc593e
hotdog16 <hotdoggg136@gmail.com>
2023-07-04 17:35:17 +0900

    구현방향 고민 중

--- backend/mqtt.js
+++ backend/mqtt.js
@@ -1,7 +1,10 @@
 const mqtt = require("mqtt");
+const WebSocket = require("ws");
 
 exports.connectToRoom = (roomId) => {
   const mqttClient = mqtt.connect("mqtt://192.168.10.102:1883"); // MQTT Broker(server)를 연결
+
+  const wd = new WebSocket("ws/localhost:8080"); // react와 nodejs를 연결하기 위해 사용
 
   mqttClient.on("connect", () => {
     console.log("MQTT connected :", mqttClient.connected);
@@ -22,6 +25,7 @@ exports.connectToRoom = (roomId) => {
 
   mqttClient.on("message", (topic, message) => {
     console.log(`보낸 메세지 ==> ${topic}: ${message.toString()}`);
+    ws.send(message.toString());
   });
 
   return mqttClient;
--- frontend/src/containers/chat/ListChatCntr.jsx
+++ frontend/src/containers/chat/ListChatCntr.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ListChatComp from "../../components/chat/ListChatComp";
 import { listChats } from "../../modules/chat/ChatMod";
-// import { useSearchParams } from "react-router-dom";
+import WebSocket from "ws";
+import mqtt from "mqtt";
 
 const ListChatsCntr = () => {
-  // const [searchParams] = useSearchParams("roomId");
+  const [messages, setMessages] = useState([]);
+  const [inputMessage, setInputMessage] = useState("");
   const dispatch = useDispatch();
   const { chats, chatError, loading, room } = useSelector(
     ({ ChatMod, RoomMod }) => ({
@@ -15,19 +17,63 @@ const ListChatsCntr = () => {
     })
   );
 
-  const onChange = (e) => {};
+  const onChange = (e) => {
+    setInputMessage(e.target.value);
+  };
 
   useEffect(() => {
-    // const room = searchParams.get("roomId");
-    console.log("ListChatCntr ==> room : ", room);
-    dispatch(listChats({ room }));
+    const mqttClient = mqtt.connect("mqtt://192.168.10.104:1883");
+    const ws = new WebSocket("ws://localhost:8080");
+    mqttClient.on("connect", () => {
+      console.log("MQTT connected : ", mqttClient.connected);
+      mqttClient.subscribe(`/room/${room}`, () => {
+        console.log(`/room/${room} 구독중`);
+      });
+    });
+    mqttClient.on("message", (topic, message) => {
+      console.log(`받은 메시지 ==> ${topic}: ${message.toString()}`);
+      const receivedMessage = JSON.parse(message.toString());
+      setMessages((prevMessages) => [...prevMessages, receivedMessage]);
+    });
+
+    ws.onmessage = (event) => {
+      console.log(`WebSocket으로 받은 메시지: ${event.data}`);
+      const receivedMessage = JSON.parse(event.data);
+      setMessages((prevMessages) => [...prevMessages, receivedMessage]);
+    };
+
+    return () => {
+      mqttClient.end();
+      ws.close();
+    };
+    // dispatch(listChats({ room }));
   }, []);
+
+  const handleSendMessage = () => {
+    const newMessage = {
+      user: "User",
+      content: inputMessage,
+    };
+
+    // MQTT로 메시지를 보냄
+    const mqttClient = mqtt.connect("mqtt://192.168.10.102:1883");
+    mqttClient.publish("/chatroom", JSON.stringify(newMessage));
+
+    // WebSocket으로 메시지를 보냄
+    const ws = new WebSocket("ws://localhost:8080");
+    ws.send(JSON.stringify(newMessage));
+
+    setInputMessage("");
+  };
+
   return (
     <ListChatComp
       chats={chats}
       onChange={onChange}
       loading={loading}
       chatError={chatError}
+      inputMessage={inputMessage}
+      onClick={handleSendMessage}
     />
   );
 };
