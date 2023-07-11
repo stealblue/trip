32faadd9949ea631d4436477a9817fbb2cbca68e
권미애 <alxo9974@naver.com>
2023-07-11 11:25:34 +0900

    게시판 이미지 업로드 기능 구현중..

--- backend/index.js
+++ backend/index.js
@@ -82,7 +82,7 @@ app.post("/img", upload.array("img"), (req, res) => {
   console.log("저장된 파일의 이름", req.file.filename);
 
   // 파일이 저장된 경로를 클라이언트에게 반환해준다.
-  const IMG_URL = `http://localhost:3000/uploads/${req.file.filename}`;
+  const IMG_URL = `http://localhost:4000/uploads/${req.file.filename}`;
   console.log(IMG_URL);
   res.json({ url: IMG_URL });
 });
