9b80db14e60c59a2a6da081550f5251c7b7c0e13
jaeho614 <jaeho614a@gmail.com>
2023-07-27 16:53:04 +0900

    0727 merge

--- frontend/src/modules/board/ReadMod.jsx
+++ frontend/src/modules/board/ReadMod.jsx
@@ -1,16 +1,25 @@
 import { createAction, handleActions } from "redux-actions";
-import createRequestSaga, { createRequestActionTypes } from "../../lib/createRequestSaga";
+import createRequestSaga, {
+  createRequestActionTypes,
+} from "../../lib/createRequestSaga";
 import * as postsAPI from "../../lib/api/posts";
 import { takeLatest } from "redux-saga/effects";
 
-const [READ_POST, READ_POST_SUCCESS, READ_POST_FAILURE] = createRequestActionTypes("post/READ_POST");
+const [READ_POST, READ_POST_SUCCESS, READ_POST_FAILURE] =
+  createRequestActionTypes("post/READ_POST");
 const UNLOAD_POST = "post/UNLOAD_POST";
-const [LIKE_POST, LIKE_POST_SUCCESS, LIKE_POST_FAILURE] = createRequestActionTypes("post/LIKE_POST"); //like
-const [IS_LIKE, IS_LIKE_SUCCESS, IS_LIKE_FAILURE] = createRequestActionTypes("post/IS_LIKE"); // like 여부
+const [LIKE_POST, LIKE_POST_SUCCESS, LIKE_POST_FAILURE] =
+  createRequestActionTypes("post/LIKE_POST"); //like
+const [IS_LIKE, IS_LIKE_SUCCESS, IS_LIKE_FAILURE] =
+  createRequestActionTypes("post/IS_LIKE"); // like 여부
 
 export const readPost = createAction(READ_POST);
 export const unloadPost = createAction(UNLOAD_POST);
-export const likePost = createAction(LIKE_POST, ({ no, id, bno }) => ({ no, id, bno }));
+export const likePost = createAction(LIKE_POST, ({ no, id, bno }) => ({
+  no,
+  id,
+  bno,
+}));
 export const isLike = createAction(IS_LIKE, ({ bno, id }) => ({ bno, id }));
 
 const readPostSaga = createRequestSaga(READ_POST, postsAPI.readPost);
@@ -27,7 +36,6 @@ const initalState = {
   post: null,
   error: null,
   like: null,
-  // like: null,
 };
 
 const ReadMod = handleActions(
@@ -50,11 +58,11 @@ const ReadMod = handleActions(
     }),
     [IS_LIKE_SUCCESS]: (state, { payload: like }) => ({
       ...state,
-      like
+      like,
     }),
     [IS_LIKE_FAILURE]: (state, { payload: error }) => ({
       ...state,
-      error
+      error,
     }),
     [UNLOAD_POST]: () => initalState,
   },
