c437f3a5e23ec73a3700fff65c596fdbbedb4083
hotdog16 <hotdoggg136@gmail.com>
2023-07-31 20:48:51 +0900

    좌석 선택 구현 중

--- frontend/src/containers/traffic/TrafficListCntr.jsx
+++ frontend/src/containers/traffic/TrafficListCntr.jsx
@@ -4,18 +4,77 @@ import { useDispatch, useSelector } from "react-redux";
 import { listTrains } from '../../modules/traffic/TrainMod';
 import { listBuses } from '../../modules/traffic/BusMod';
 import LoadingComp from '../../components/common/LoadingComp';
+import Swal from 'sweetalert2';
 
 const TrafficListCntr = () => {
+
   const onTicketing = (e) => {
-    console.log('currentTarget : ', e.currentTarget);
-    console.log('Target : ', e.target);
-    console.log('data-item : ', e.currentTarget.dataset.item);
+    // console.log('currentTarget : ', e.currentTarget);
+    // console.log('Target : ', e.target);
+    // console.log('data-item : ', e.currentTarget.dataset.item);
+    console.log('안들어오나')
     const item = (e.currentTarget.dataset.item);
-    console.log('item -------: ', item);
-    // for (let i in item) {
-    //   console.log('item : ', item[i]);
-    // }
+    const jsonItem = JSON.parse(item);
+    console.log('item : ', item);
+    Swal.fire({
+      title: '예매 진행할까요?',
+      icon: 'question',
+      showCancelButton: true,
+      cancelButtonText: 'CANCEL',
+    })
+      .then((res) => {
+        if (res.isConfirmed) {
+          Swal.fire({
+            title: `${jsonItem.depplacename ? jsonItem.depplacename + "역" : jsonItem.depPlaceNm + "정류장"}에서 출발하는 인원을 선택해주세요.`,
+            input: 'number',
+            inputAttributes: {
+              min: 1,
+              max: 5
+            },
+            showCancelButton: true,
+            cancelButtonText: 'CANCEL',
+          })
+            .then((result) => {
+              const seatData = [
+                ['A1', 'A2', 'A3', 'A4'], ['B1', 'B2', 'B3', 'B4'], ['C1', 'C2', 'C3', "C4"],
+                ['D1', 'D2', 'D3', 'D4'], ['E1', 'E2', 'E3', 'E4'], ['F1', 'F2', 'F3', "F4"],
+                ['G1', 'G2', 'G3', 'G4'], ['H1', 'H2', 'H3', 'H4'], ['I1', 'I2', 'I3', "I4"],
+                ["J1", 'J2', 'J3', 'J4']
+              ];
+              const checkBoxData = seatData.map((row) =>
+                row.map((item) => ({
+                  input: 'checkbox',
+                  title: item
+                }))
+              );
+              if (result.isConfirmed) {
+                Swal.fire({
+                  icon: 'warning',
+                  title: '좌석 선택 방법 고민 중',
+                  html: generateTableHtml(checkBoxData)
+                })
+              }
+            })
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      })
   }
+
+  function generateTableHtml(data) {
+    let html = '<table>';
+    data.forEach((row, rowIndex) => {
+      html += '<tr>';
+      row.forEach((checkbox, colIndex) => {
+        html += `<td><label><input id="swal-checkbox-${rowIndex}-${colIndex}" type="checkbox">${checkbox.title}</label></td>`;
+      });
+      html += '</tr>';
+    });
+    html += '</table>';
+    return html;
+  }
+
 
   const dispatch = useDispatch();
   const { resultTrains, resultBuses, pageNoTrain, pageNoBus, dateTrain, dateBus, startStation, startTerminal, endStation, endTerminal, loading } = useSelector(({ BusMod, TrainMod, LoadingMod }) => ({
@@ -59,7 +118,7 @@ const TrafficListCntr = () => {
   return (
     <div>
       {pageNoTrain && startStation && endStation && dateTrain && <TrafficListComp resultTrains={resultTrains} loading={loading} onTicketing={onTicketing} />}
-      {pageNoBus && startTerminal && endTerminal && dateBus && <TrafficListComp resultBuses={resultBuses} loading={loading} />}
+      {pageNoBus && startTerminal && endTerminal && dateBus && <TrafficListComp resultBuses={resultBuses} loading={loading} onTicketing={onTicketing} />}
     </div>
   );
 };
